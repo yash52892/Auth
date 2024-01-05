28a7c0d332242f5e524a3e060e9b3ccc1fb4135b
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-05 23:26:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import TokenProvider from './store/TokenProvider';
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <TokenProvider>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </TokenProvider>
 );

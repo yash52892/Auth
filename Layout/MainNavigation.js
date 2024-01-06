346b717bbcd918965934f1b4dca42eb817aa8cd2
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-06 17:33:01 +0530

    logout re-direct is added

--- Layout/MainNavigation.js
+++ Layout/MainNavigation.js
@@ -2,13 +2,17 @@ import { Link } from 'react-router-dom';
 import TokenContext from '../../store/tokenContext';
 import classes from './MainNavigation.module.css';
 import { useContext } from 'react';
+import {useHistory} from 'react-router-dom';
+
 
 const MainNavigation = () => {
   const tok=useContext(TokenContext);
+  const history=useHistory();
   const handleLogout=()=>{
     tok.handTok(null);
+    history.replace("/auth")
   }
-  return (  
+  return (
     <header className={classes.header}>
       <Link to='/'>
         <div className={classes.logo}>React Auth</div>

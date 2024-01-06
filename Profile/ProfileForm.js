b7fd3245f707094a42117fc9fd0efed136cdd391
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-06 17:27:41 +0530

    Update ProfileForm.js

--- Profile/ProfileForm.js
+++ Profile/ProfileForm.js
@@ -1,14 +1,39 @@
 import classes from './ProfileForm.module.css';
+import { useContext, useRef } from 'react';
+import TokenContext from '../../store/tokenContext';
+import {useHistory} from 'react-router-dom';
 
 const ProfileForm = () => {
+
+  const tok=useContext(TokenContext);
+  const pass=useRef(null);
+  const history=useHistory();
+  console.log(tok);
+  
+  const handlePassChange=()=>{
+    const newEnteredpass=pass.current.value;
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBTSDcMIiQIhAgney8KSc7Iurf0R3PPKFI',{
+      method: "POST",
+      body: JSON.stringify({
+        idToken: tok.token,
+        password: newEnteredpass,
+        returnSecureToken: true
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(res=>{
+      history.replace('/'); 
+    });
+  }
   return (
     <form className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={pass}/>
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button onClick={handlePassChange}>Change Password</button>
       </div>
     </form>
   );

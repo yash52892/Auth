7b5d281b8ab32bd78253bed855a2a8168c6422c5
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-06 22:08:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
 import { Switch, Route } from 'react-router-dom';
-
+import TokenContext from './store/tokenContext';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import { useContext } from 'react';
 
 function App() {
+const tok=useContext(TokenContext);
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+       {!tok.isLoggedin && (
+       <Route path='/auth'>
           <AuthPage />
         </Route>
+        )}
         <Route path='/profile'>
-          <UserProfile />
+        {tok.isLoggedin && <UserProfile />}
+        {!tok.isLoggedin && <Redirect to='/auth'/>}
+        </Route>
+        <Route path='*'>
+          <Redirect to='/'/>
         </Route>
       </Switch>
     </Layout>
